788e9da3aa51d42e5cdb2a178d1e0fe071842904
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-06-06 10:54:39 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import Constants from 'expo-constants';
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId,
-  databaseURL: Constants.manifest.extra.databaseURL
+  apiKey: Constants.manifest.extraKeys.apiKey,
+  authDomain: Constants.manifest.extraKeys.authDomain,
+  projectId: Constants.manifest.extraKeys.projectId,
+  storageBucket: Constants.manifest.extraKeys.storageBucket,
+  messagingSenderId: Constants.manifest.extraKeys.messagingSenderId,
+  appId: Constants.manifest.extraKeys.appId,
+  databaseURL: Constants.manifest.extraKeys.databaseURL
 };
 initializeApp(firebaseConfig);
 export const auth = getAuth();
