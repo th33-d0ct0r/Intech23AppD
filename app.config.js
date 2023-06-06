7d9da687e26b01d11602c9235bdb1ae18f3cc1ed
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-06-06 10:53:59 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,35 +1,50 @@
 import 'dotenv/config';
 
 
-export default {
-  "expo": {
-    "name": "ChatApp0",
-    "slug": "ChatApp0",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
+export default ({config}) => {
+  const appConfig = ({
+    ...config,
+    version: process.env.VERSION,
+    //override anything you want
+    "expo": {
+      "android": {
+        "package": "com.bhavit_grover_6220.ChatApp"
+      },
+      "extra": {
+        "eas": {
+          "projectId": "6f77bda4-1a0c-498c-afdf-a4a324078194"
+        }
+      },
+      "owner": "bhavit_grover_6220",
+      "name": "ChatApp",
+      "slug": "ChatApp",
+      "version": "1.0.0",
+      "orientation": "portrait",
+      "icon": "./assets/icon.png",
+      "splash": {
+        "image": "./assets/splash.png",
+        "resizeMode": "contain",
         "backgroundColor": "#ffffff"
-      }
-    },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    },
-    extra: {
+      },
+      "updates": {
+        "fallbackToCacheTimeout": 0
+      },
+      "assetBundlePatterns": [
+        "**/*"
+      ],
+      "ios": {
+        "supportsTablet": true
+      },
+      "android": {
+        "adaptiveIcon": {
+          "foregroundImage": "./assets/adaptive-icon.png",
+          "backgroundColor": "#FFFFFF"
+        }
+      },
+      "web": {
+        "favicon": "./assets/favicon.png"
+      },
+    extraKeys: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
@@ -37,5 +52,6 @@ export default {
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID
     }
-  }
+  }});
+  return appConfig;
 }
